861a417d856d012aa7f205531c699987fb71d29a
jonyluke <jony8mb@gmail.com>
2019-07-07 12:18:26 +0200

    Added Switch to PC option

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Undertale-Save-Converter
-This tool allows you to convert your Undertale PC save to switch version, in the future it will convert from Switch to PC
+This tool allows you to convert your Undertale PC save to switch version and viceversa
 ## Requirements
+- Python3
+
+PC TO SWITCH
 - file0
 - file9
 - undertale.ini
 (By default they are stored in AppData\Local\UNDERTALE\)
-- Python3
+
+SWITCH TO PC
+- undertale.sav
+
 ## How to use
-Put the three files inside the folder and execute converter.py, it will make an undertale.sav file that you can use in Nintendo Switch
+Put the necessary files inside the folder and execute converter.py, it will make the files you need
--- converter.py
+++ converter.py
@@ -1,9 +1,4 @@
-f1 = open("file9","r")
-f2 = open("file0","r")
-f3 = open("undertale.ini","r")
-final = open("undertale.sav","w")
-        
-def add(f,final):
+def add(f, final):
     for i in f:
         i = i.replace("\n", "")
         i = i.replace(" ", "")
@@ -13,33 +8,107 @@ def add(f,final):
     for i in f:
         i = i.replace("\n", "")
         i = i.replace(" ", "")
-        final.write('\\n'+ i + '\\r')
-    size=final.tell()             
-    final.truncate(size-2)
-    final.seek(size-2,0)
-    final.write('"')
+        final.write('\\n' + i + '\\r')
+        size = final.tell()
+        final.truncate(size-2)
+        final.seek(size-2, 0)
+        final.write('"')
 
-final.write('{ "default": "", "file9": "')
 
-add(f1,final)
+def add2(file, file2):
+    while True:
+        char = file2.read(1)
+        if not char:
+            break
+        if char == "\\":
+            while file2.read(1) != "n":
+                pass
+            file.write("\n")
+        elif char == '"':
+            break
+        else:
+            file.write(char)
 
-final.write(', "config.ini": "", "undertale.ini": "')
-for i in f3:
-    i = i.replace("\n", "")
-    i = i.replace(" ", "")
-    if i.startswith("["):
-        final.write(i + '\\r')
-    else:
-        r = i.split("=")
-        p = r[1].split('"')
-        p = p[1] 
-        final.write('\\n'+ r[0] + '=\\"' + p + '\\"' + '\\r')
-        
-final.write('\\n", "file0": "')
-add(f2,final)
-final.write(chr(32) + chr(125) + chr(0))
+    file.close()
 
-f1.close()
-f2.close()
-f3.close()
-final.close()
+
+def pc_to_switch():
+    f1 = open("file9", "r")
+    f2 = open("file0", "r")
+    f3 = open("undertale.ini", "r")
+    final = open("undertale.sav", "w")
+
+    final.write('{ "default": "", "file9": "')
+
+    add(f1, final)
+
+    final.write(', "config.ini": "", "undertale.ini": "')
+    for i in f3:
+        i = i.replace("\n", "")
+        i = i.replace(" ", "")
+        if i.startswith("["):
+            final.write(i + '\\r')
+        else:
+            r = i.split("=")
+            p = r[1].split('"')
+            p = p[1]
+            final.write('\\n' + r[0] + '=\\"' + p + '\\"' + '\\r')
+
+    final.write('\\n", "file0": "')
+    add(f2, final)
+    final.write(chr(32) + chr(125) + chr(0))
+
+    f1.close()
+    f2.close()
+    f3.close()
+    final.close()
+
+
+def switch_to_pc():
+    f = open("file9", "w+")
+    f2 = open("undertale.sav", "r")
+    f2.seek(27)
+    f3 = open("undertale.ini", "w+")
+    f4 = open("file0", "w+")
+
+    add2(f, f2)
+
+    f2.seek(f2.tell() + 38)
+
+    while True:
+        char = f2.read(1)
+        if not char:
+            break
+        if char == "\\":
+            n = f2.read(1)
+            if n != '"':
+                while f2.read(1) != "n":
+                    pass
+                f3.write("\n")
+            else:
+                f3.write('"')
+
+        elif char == ",":
+            size = f3.tell()
+            f3.truncate(size-1)
+            f3.seek(size-1, 0)
+            break
+        else:
+            f3.write(char)
+
+    f3.close()
+
+    f2.seek(f2.tell() + 11)
+
+    add2(f4, f2)
+
+
+print("Undertale-Save-Converter")
+print("1) Pc to Switch")
+print("2) Switch to PC")
+selection = input("> ")
+
+if selection == 1:
+    pc_to_switch()
+elif selection == 2:
+    switch_to_pc()
